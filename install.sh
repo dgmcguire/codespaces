8c70a75f2ed64345494806d4ffac89e1a28f2e20
dan <dgmcguire@test.com>
2025-03-17 07:20:52 -0500

    install lua/elixir lsp servers

--- install.sh
+++ install.sh
@@ -9,13 +9,15 @@ apk add --no-cache \
 
 mkdir -p ~/lsp/{lua,elixir}
 
-if [ -z "$( ls -A '~/lsp/lua' )" ]; then
+if [ -z "$( ls -A ~/lsp/lua )" ]; then
   wget https://github.com/LuaLS/lua-language-server/releases/download/3.13.9/lua-language-server-3.13.9-linux-x64-musl.tar.gz
   tar -xzvf lua-language-server-3.13.9-linux-x64-musl.tar.gz -C ~/lsp/lua/
   rm lua-language-server-3.13.9-linux-x64-musl.tar.gz 
 fi
-if [ -z "$( ls -A '~/lsp/elixir' )" ]; then
+if [ -z "$( ls -A ~/lsp/elixir )" ]; then
   wget https://github.com/elixir-lsp/elixir-ls/releases/download/v0.27.1/elixir-ls-v0.27.1.zip
   unzip elixir-ls-v0.27.1.zip -d ~/lsp/elixir
   rm elixir-ls-v0.27.1.zip
 fi
+
+git clone "git clone https://oauth2:$(GITLAB_TOKEN)@gitlab.com:dgmcguire/nixconfig.git"

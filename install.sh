e84b5ea49c3c777f9f97422d001b05e5cd34b412
dan <dgmcguire@test.com>
2025-03-17 10:43:19 -0500

    fix conditional

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ -z "$( ls -A ~/lsp/lua )" ]; then
   tar -xzvf lua-language-server-3.13.9-linux-x64-musl.tar.gz -C ~/lsp/lua/
   rm lua-language-server-3.13.9-linux-x64-musl.tar.gz 
 
-  if [ ! grep "LUA_LSP" ~/.zshrc ]; then
+  if [ ! $(grep "LUA_LSP" ~/.zshrc) ]; then
     echo "export LUA_LSP='$HOME/lsp/lua/bin/lua-language-server'" >> ~/.zshrc
   fi
 fi
@@ -24,7 +24,7 @@ if [ -z "$( ls -A ~/lsp/elixir )" ]; then
   unzip elixir-ls-v0.27.1.zip -d ~/lsp/elixir
   rm elixir-ls-v0.27.1.zip
 
-  if [ ! grep "ELIXIR_LSP" ~/.zshrc ]; then
+  if [ ! $(grep "ELIXIR_LSP" ~/.zshrc) ]; then
     echo "export ELIXIR_LSP='$HOME/lsp/elixir/language_server.sh'" >> ~/.zshrc
   fi
 fi

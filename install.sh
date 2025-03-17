428c19ba835ab598322207eb4b5e41d8fe97a191
dan <dgmcguire@test.com>
2025-03-17 10:16:17 -0500

    exports lsp env

--- install.sh
+++ install.sh
@@ -13,14 +13,20 @@ if [ -z "$( ls -A ~/lsp/lua )" ]; then
   wget https://github.com/LuaLS/lua-language-server/releases/download/3.13.9/lua-language-server-3.13.9-linux-x64-musl.tar.gz
   tar -xzvf lua-language-server-3.13.9-linux-x64-musl.tar.gz -C ~/lsp/lua/
   rm lua-language-server-3.13.9-linux-x64-musl.tar.gz 
-  echo "export LUA_LSP='$HOME/lsp/lua/bin/lua-language-server'" >> ~/.zshrc
+
+  if [ ! grep "LUA_LSP" ~/.zshrc ]; then
+    echo "export LUA_LSP='$HOME/lsp/lua/bin/lua-language-server'" >> ~/.zshrc
+  fi
 fi
 
 if [ -z "$( ls -A ~/lsp/elixir )" ]; then
   wget https://github.com/elixir-lsp/elixir-ls/releases/download/v0.27.1/elixir-ls-v0.27.1.zip
   unzip elixir-ls-v0.27.1.zip -d ~/lsp/elixir
   rm elixir-ls-v0.27.1.zip
-  echo "export ELIXIR_LSP='$HOME/lsp/elixir/language_server.sh'" >> ~/.zshrc
+
+  if [ ! grep "ELIXIR_LSP" ~/.zshrc ]; then
+    echo "export ELIXIR_LSP='$HOME/lsp/elixir/language_server.sh'" >> ~/.zshrc
+  fi
 fi
 
 git clone "https://dgmcguire:$GITLAB_TOKEN@gitlab.com/dgmcguire/nixconfig.git" ~/nixconfig

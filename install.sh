093012bab6363625a8dd756a919ee5252d3c7782
dan <dgmcguire@test.com>
2025-03-17 06:16:25 -0500

    install lua/elixir lsp servers

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# nvim deps
 apk add --no-cache \
-	jq \
-	lua-language-server \
 	gcc \
 	unzip \
 	make \
 	nerd-fonts 
+
+mkdir ~/lsp/{lua,elixir}
+
+if [ -z "$( ls -A '~/lsp/lua' )" ]; then
+  wget https://github.com/LuaLS/lua-language-server/releases/download/3.13.9/lua-language-server-3.13.9-linux-x64-musl.tar.gz
+  tar -xzvf lua-language-server-3.13.9-linux-x64-musl.tar.gz -C ~/lsp/lua/
+  rm lua-language-server-3.13.9-linux-x64-musl.tar.gz 
+fi
+if [ -z "$( ls -A '~/lsp/elixir' )" ]; then
+  wget https://github.com/elixir-lsp/elixir-ls/releases/download/v0.27.1/elixir-ls-v0.27.1.zip
+  unzip elixir-ls-v0.27.1.zip -d ~/lsp/elixir
+  rm elixir-ls-v0.27.1.zip
+fi

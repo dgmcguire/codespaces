07d41051d30c999179eec162ed6c291e950cead7
dan <dgmcguire@test.com>
2025-03-20 08:24:58 -0500

    fix some aliases

--- scripts/zsh_functions_and_aliases.zsh
+++ scripts/zsh_functions_and_aliases.zsh
@@ -1,3 +1,3 @@
 alias dockerpsf='docker ps --format "table {{.Names}} {{.Status}} {{.Ports}}"'
-alias ta="tmux -2 -u new-session -a -s ";
+alias ta="tmux new-session -A -s";
 alias tl="tmux -u list-sessions";

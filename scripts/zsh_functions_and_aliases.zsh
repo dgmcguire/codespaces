7bf8330e46cabc842422fa000c99857e4677484c
dan <dgmcguire@test.com>
2025-03-24 07:24:00 -0500

    adds tabs to dockerpsf

--- scripts/zsh_functions_and_aliases.zsh
+++ scripts/zsh_functions_and_aliases.zsh
@@ -1,3 +1,3 @@
-alias dockerpsf='docker ps --format "table {{.Names}} {{.Status}} {{.Ports}}"'
+alias dockerpsf='docker ps --format "table {{.Names}}	{{.Status}}	{{.Ports}}"'
 alias ta="tmux new-session -A -s";
 alias tl="tmux -u list-sessions";

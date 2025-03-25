57f5ddcb98d865381d60d302c22926de89908f65
dan <dgmcguire@test.com>
2025-03-25 12:48:42 -0500

    adds psql and csinstall func

--- scripts/zsh_functions_and_aliases.zsh
+++ scripts/zsh_functions_and_aliases.zsh
@@ -3,10 +3,11 @@ alias ta="tmux new-session -A -s";
 alias tl="tmux -u list-sessions";
 
 function csinstall() {
-  if [ -f  "/workspaces/.codespaces/.persistedshare/dotfiles/install.sh" ]; then
+  if [ -f "/workspaces/.codespaces/.persistedshare/dotfiles/install.sh" ]; then
     echo "installing dotfiles"
     $(cd /workspaces/.codespaces/.persistedshare/dotfiles; git pull; ./install.sh)
   else
+    ls  /workspaces/.codespaces/.persistedshare/dotfiles
     echo "dotfiles not found"
   fi
 }
